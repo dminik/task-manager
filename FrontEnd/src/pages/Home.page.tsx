d4bacc2241e640faf520cee596be5442dd619939
Dmytro Nikulin <dnikulin@optimalplanning.com>
2024-10-22 13:49:32 -0700

    Added packages and nav code

--- FrontEnd/src/pages/Home.page.tsx
+++ FrontEnd/src/pages/Home.page.tsx
@@ -1,14 +1,12 @@
 import TaskManager from '@/components/TaskManager/TaskManager';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
+import { HomePageLayout } from '@/components/HomePageLayout/HomePageLayout';
 
 export function HomePage() {
   return (
     <>
-      <ColorSchemeToggle />
-      <Welcome />
-      <TaskManager />
-
+      <HomePageLayout />
     </>
   );
 }
