8b10bbd4dd0b1dabfb5008e456e8c3882477e80d
Dmytro Nikulin <dnikulin@optimalplanning.com>
2024-10-22 14:49:27 -0700

    Extracted NavBar

---
+++ FrontEnd/src/components/HomePage/NavBar/NavBar.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import {
+  IconBellRinging,
+  IconReceipt2,
+  IconSwitchHorizontal,
+  IconLogout,
+} from '@tabler/icons-react';
+import classes from './NavbarSimple.module.css';
+
+const data = [
+  { link: '', label: 'Notifications', icon: IconBellRinging },
+  { link: '', label: 'Billing', icon: IconReceipt2 },
+];
+
+export function NavLayout() {
+  const [active, setActive] = useState('Billing');
+
+  const links = data.map((item) => (
+    <a
+      className={classes.link}
+      data-active={item.label === active || undefined}
+      href={item.link}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(item.label);
+      }}
+    >
+      <item.icon className={classes.linkIcon} stroke={1.5} />
+      <span>{item.label}</span>
+    </a>
+  ));
+
+  return (
+    <nav className={classes.navbar}>
+      <div className={classes.navbarMain}>
+        {links}
+      </div>
+
+      <div className={classes.footer}>
+        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+          <IconSwitchHorizontal className={classes.linkIcon} stroke={1.5} />
+          <span>Change account</span>
+        </a>
+
+        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+          <IconLogout className={classes.linkIcon} stroke={1.5} />
+          <span>Logout</span>
+        </a>
+      </div>
+    </nav>
+  );
+}
