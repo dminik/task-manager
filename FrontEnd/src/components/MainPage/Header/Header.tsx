ffe5bf8181d261cf19f8bf3f9e0d88b78e40f6fd
Dmytro Nikulin <dnikulin@optimalplanning.com>
2024-10-28 20:09:56 -0700

    Added Header sub-menu

--- FrontEnd/src/components/MainPage/Header/Header.tsx
+++ FrontEnd/src/components/MainPage/Header/Header.tsx
@@ -1,11 +1,13 @@
-import { IconBell, IconSettings } from '@tabler/icons-react';
+import { IconBell, IconFaceId, IconLogin, IconLogout, IconPointFilled, IconSearch, IconSettings } from '@tabler/icons-react';
 import { Link } from 'react-router-dom';
-import { Burger, Group } from '@mantine/core';
+import { Burger, Group, Menu, } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import FastbreakLogo from '@/components/Icons/FastbreakLogo/FastbreakLogo';
+import classes from './Header.module.css';
 
 export function Header() {
   const [opened, { toggle }] = useDisclosure();
+  const userName = 'Sarah';
 
   return (
     <>
@@ -25,12 +27,45 @@ export function Header() {
           </Link>
         </Group>
         <Group>
-          <Link to="#">
-            <IconBell size={18} />
-          </Link>
-          <Link to="#">
-            <IconSettings size={18} />
-          </Link>
+          <Menu>
+            <Menu.Target >
+              <IconBell size={18} />
+            </Menu.Target>
+
+            <Menu.Dropdown>
+              <Menu.Item
+                leftSection={<IconFaceId size={18} />}
+              >
+                Name action 1<span className={classes.notificationDate}>2 days ago</span><IconPointFilled size={18} />
+              </Menu.Item>
+              <Menu.Item
+                leftSection={<IconFaceId size={18} />}
+              >
+                Name action 2<span className={classes.notificationDate}>2 days ago</span>
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+
+          <Menu>
+            <Menu.Target>
+              <IconFaceId size={18} />
+            </Menu.Target>
+
+            <Menu.Dropdown>
+              <Menu.Item
+                leftSection={<IconSettings size={18} />}
+              >
+                Settings
+              </Menu.Item>
+              <Menu.Item
+                leftSection={userName ? <IconLogin size={18} /> : <IconLogout size={18} />}
+              >
+                {userName ? 'Log Out' : 'Log In'}
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+          <span>{userName}</span>
+
         </Group>
       </div>
     </>
